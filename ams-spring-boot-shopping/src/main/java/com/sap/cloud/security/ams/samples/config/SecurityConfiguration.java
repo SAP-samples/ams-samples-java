06bcd0e033841ff053d4f48d2dcaadd61bf1595e
Manuel Fink <manuel.fink@sap.com>
2026-01-21 16:14:21 +0100

    add route security parameter

--- ams-spring-boot-shopping/src/main/java/com/sap/cloud/security/ams/samples/config/SecurityConfiguration.java
+++ ams-spring-boot-shopping/src/main/java/com/sap/cloud/security/ams/samples/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.sap.cloud.security.ams.samples.config;
 
+import com.sap.cloud.security.ams.spring.authorization.AmsRouteSecurity;
 import com.sap.cloud.security.spring.config.IdentityServicesPropertySourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +33,7 @@ import static org.springframework.http.HttpMethod.*;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AmsRouteSecurity via) throws Exception {
         http.authorizeHttpRequests(authz -> {
                     // Public endpoints - Spring Boot Actuator health check
                     authz.requestMatchers(GET, "/actuator/health").permitAll();
